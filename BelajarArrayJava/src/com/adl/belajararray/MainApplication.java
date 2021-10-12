469ff65c0a7bb17a93f5cad929ffe936e9f816be
dewabrata <dewabrata@gmail.com>
2021-10-12 14:18:56 +0700

    update dewa

--- BelajarArrayJava/src/com/adl/belajararray/MainApplication.java
+++ BelajarArrayJava/src/com/adl/belajararray/MainApplication.java
@@ -4,8 +4,9 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World Agung");
+		System.out.println("Hello World dewa");
 		System.out.println("Ok");
+		System.out.println("test");
 	}
 
 }
