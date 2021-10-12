0bfb8f304c5489bae1bf5abc6e56cc3e638fe99a
dewabrata <dewabrata@gmail.com>
2021-10-12 15:24:56 +0700

    add tugas input model

--- BelajarArrayJava/src/com/adl/belajararray/MainApplication.java
+++ BelajarArrayJava/src/com/adl/belajararray/MainApplication.java
@@ -1,20 +1,100 @@
 package com.adl.belajararray;
 
+import java.util.Scanner;
+
 public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World dewa");
-
-		System.out.println("Hello World Agung Setiawan");
-
-		System.out.println("Ok");
-
-		System.out.println("test");
-
-		System.out.println("ADL Bootcamp");
+		MainApplication main = new MainApplication();
+		main.insertData();
+		
 
 	}
+	
+	public void insertData() {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		
+		
+		System.out.print("Masukan banyak data : ");
 
+		int jmlData = scan.nextInt();
+		
+		ModelData[] data  = new ModelData[jmlData];
+		
+		
+		for (int i = 0; i < jmlData; i++) {
+			data[i] = new ModelData();
+			System.out.print("Masukan nama anda :");
+			String nama = scan.next();
+			
+			data[i].setNama(nama);
+			
+			System.out.print("Masukan alamat anda :");
+			String alamat = scan.next();
+			data[i].setAlamat(alamat);
+		}
+		
+		//cara for each
+		for (ModelData modelData : data) {
+			
+			System.out.println("=============================");
+			System.out.println("Nama :" + modelData.getNama());
+			System.out.println("Alamat :" + modelData.getAlamat());
+			System.out.println("=============================");
+			
+		}
+		
+		//cara for next
+		for (int i = 0; i < jmlData; i++) {
+			System.out.println("=============================");
+			System.out.println("Nama :" + data[i].getNama());
+			System.out.println("Alamat :" + data[i].getAlamat());
+			System.out.println("=============================");
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+	
+	/*
+	 * Masukan jumlah mahasiswa : 2
+	 * 
+	 * =============================
+	 * Masukan nama mahasiswa : Hanif
+	 * Masukan nik mahasiswa : 12345
+	 * Masukan nilai semester1 : 9
+	 * Masukan nilai semester2 : 9
+	 * Masukan nilai semester3 : 9
+	 * =============================
+	 * 
+	 *==============================
+	 * Masukan nama mahasiswa : Aqil
+	 * Masukan nik mahasiswa : 2345
+	 * Masukan nilai semester1 : 7
+	 * Masukan nilai semester2 : 7
+	 * Masukan nilai semester3 : 7
+	 * =============================
+	 * 
+	 * 
+	 *=============================
+	 *Nama        | Nik   | Nilai Rata 
+	 *Hanif       | 12345 |  9
+	 *Aqil        | 2345  |  7
+	 *
+	 *Nama Nik nilai1-3 dan rata2 dalam satu Model --> ModelMahasiswa
+	 *
+	 */
+	public void insertDataMahasiswa() {
+		
+	}
 }
---
+++ BelajarArrayJava/src/com/adl/belajararray/ModelData.java
@@ -0,0 +1,29 @@
+package com.adl.belajararray;
+
+public class ModelData {
+	
+	private String nama;
+	private String alamat;
+	
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
