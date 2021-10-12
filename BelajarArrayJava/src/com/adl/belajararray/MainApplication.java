796bea2ec2324aae2e10ac32f3dbb560f47f18d1
Agung <Agung@DESKTOP-S20TCD6>
2021-10-12 14:18:24 +0700

    Update Agung1

--- BelajarArrayJava/src/com/adl/belajararray/MainApplication.java
+++ BelajarArrayJava/src/com/adl/belajararray/MainApplication.java
@@ -4,8 +4,9 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World Agung");
+		System.out.println("Hello World Agung Setiawan");
 		System.out.println("Ok");
+		System.out.println("ADL Bootcamp");
 	}
 
 }
